60843ca76ddc7b2399a721317c8dd309349ddcd4
MuhammedFaizalB <MuhammedFaizalB284@gmail.com>
2022-02-14 02:00:36 +0530

    Delete Button Added in Transaction Page

--- lib/Screens/transaction/transaction.dart
+++ lib/Screens/transaction/transaction.dart
@@ -1,33 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:intl/intl.dart';
+import 'package:money_management/db/category/category_db.dart';
+import 'package:money_management/db/transaction/transaction_db.dart';
+import 'package:money_management/models/category/category_model.dart';
+import 'package:money_management/models/transaction/transaction_model.dart';
 
 class Transaction extends StatelessWidget {
   const Transaction({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        padding: const EdgeInsets.all(10),
-        itemBuilder: (ctx, index) {
-          return Card(
-            elevation: 2,
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
-                child: Text(
-                  '1$index\n Feb',
-                  textAlign: TextAlign.center,
+    TransactionDB.instance.refreshUI();
+    CategoryDb.instance.refreshUI();
+    return ValueListenableBuilder(
+      valueListenable: TransactionDB.instance.transactionListNodifier,
+      builder: (BuildContext ctx, List<TransactionModel> newList, Widget? _) {
+        return ListView.separated(
+            padding: const EdgeInsets.all(10),
+            itemBuilder: (ctx, index) {
+              final _value = newList[index];
+              return Slidable(
+                key: Key(_value.id!),
+                startActionPane: ActionPane(
+                  motion: BehindMotion(),
+                  children: [
+                    SlidableAction(
+                      onPressed: (ctx) {
+                        TransactionDB.instance.deleteTransaction(_value.id!);
+                      },
+                      icon: Icons.delete_forever_outlined,
+                      label: 'Delete',
+                      backgroundColor: Colors.red,
+                    )
+                  ],
                 ),
-              ),
-              title: const Text('Food'),
-              subtitle: const Text('100'),
-            ),
-          );
-        },
-        separatorBuilder: (ctx, index) {
-          return const SizedBox(
-            height: 10,
-          );
-        },
-        itemCount: 10);
+                child: Card(
+                  elevation: 2,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Text(
+                        parseDate(_value.date),
+                        textAlign: TextAlign.center,
+                      ),
+                      backgroundColor: _value.type == CategoryType.income
+                          ? Colors.green[800]
+                          : Colors.red[800],
+                    ),
+                    title: Text(_value.category.name),
+                    subtitle: Text('RS ${_value.amount}'),
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (ctx, index) {
+              return const SizedBox(
+                height: 10,
+              );
+            },
+            itemCount: newList.length);
+      },
+    );
+  }
+
+  String parseDate(DateTime date) {
+    final _date = DateFormat.MMMd().format(date);
+    final _splitDate = _date.split(' ');
+    return '${_splitDate.last}\n${_splitDate.first}';
   }
 }
--- lib/db/transaction/transaction_db.dart
+++ lib/db/transaction/transaction_db.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 
 import 'package:money_management/models/transaction/transaction_model.dart';
@@ -6,6 +7,8 @@ const transaction_db_name = 'transaction-db';
 
 abstract class TransactionDbFunction {
   Future<void> addTransaction(TransactionModel obj);
+  Future<List<TransactionModel>> getAllTransaction();
+  Future<void> deleteTransaction(String id);
 }
 
 class TransactionDB implements TransactionDbFunction {
@@ -16,9 +19,34 @@ class TransactionDB implements TransactionDbFunction {
   factory TransactionDB() {
     return instance;
   }
+
+  ValueNotifier<List<TransactionModel>> transactionListNodifier =
+      ValueNotifier([]);
+
   @override
   Future<void> addTransaction(TransactionModel obj) async {
     final _db = await Hive.openBox<TransactionModel>(transaction_db_name);
     await _db.put(obj.id, obj);
+  }
+
+  Future<void> refreshUI() async {
+    final _list = await getAllTransaction();
+    _list.sort((first, second) => second.date.compareTo(first.date));
+    transactionListNodifier.value.clear();
+    transactionListNodifier.value.addAll(_list);
+    transactionListNodifier.notifyListeners();
+  }
+
+  @override
+  Future<List<TransactionModel>> getAllTransaction() async {
+    final _db = await Hive.openBox<TransactionModel>(transaction_db_name);
+    return _db.values.toList();
+  }
+
+  @override
+  Future<void> deleteTransaction(String id) async {
+    final _db = await Hive.openBox<TransactionModel>(transaction_db_name);
+    _db.delete(id);
+    refreshUI();
   }
 }
